3f75258767f07f9c882e21e5680be28f3e1efda4
nelson-XILV <khojugken@gmail.com>
2022-11-08 17:52:34 +0300

    added stub buttons on the toolbar

--- app/src/main/java/com/xilv/addnewitemrecview/MainActivity.kt
+++ app/src/main/java/com/xilv/addnewitemrecview/MainActivity.kt
@@ -3,6 +3,9 @@ package com.xilv.addnewitemrecview
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.GridLayoutManager
@@ -18,6 +21,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        /*
+        supportActionBar - это и есть Toolbar
+        setDisplayHomeAsUpEnabled - активирует стрелку, которая находится в Toolbar
+         */
+        supportActionBar?.setDisplayHomeAsUpEnabled(false)
+
+        /*
+        title позволяет изменить надпись в Toolbar
+         */
+        supportActionBar?.title = "Maestro"
+
         init()
 
         /*
@@ -28,6 +43,38 @@ class MainActivity : AppCompatActivity() {
                 adapter.addMaestro(it.data?.getSerializableExtra("maestro") as Maestro)
             }
         }
+    }
+
+    /*
+    данная функция позволяет загрузить main_menu в память
+     */
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main_activity, menu)
+        return true
+    }
+
+    /*
+    слушатель нажатий для элементов Toolbar
+     */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // прослушиваем нажатия на элементы в Toolbar
+        when (item.itemId) {
+            // android.R.id.home -> finish()
+            R.id.refresh_menu -> {
+                Toast.makeText(this, "Refresh", Toast.LENGTH_SHORT).show()
+            }
+            R.id.delete_menu -> {
+                Toast.makeText(this, "Delete", Toast.LENGTH_SHORT).show()
+            }
+            R.id.search_menu -> {
+                Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show()
+            }
+            R.id.save_menu -> {
+                Toast.makeText(this, "Save", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        return true
     }
 
     private fun init() {
